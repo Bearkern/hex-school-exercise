1ef0df5a0687a8f1565c120a322ed0bb02260005
Bearkern <haolin1224@gmail.com>
2016-12-07 20:37:41 +0800

    cart finished

--- RWD/9-final-assignment/js/all.js
+++ RWD/9-final-assignment/js/all.js
@@ -3,4 +3,28 @@ $(document).ready(function() {
 		e.preventDefault();
 		$('body').toggleClass('menu-show');
 	});
+
+	$('#wishlist1').on('click', function(e){
+		e.preventDefault();
+		$('#wishlist1').toggleClass('fa-heart-o');
+		$('#wishlist1').addClass('fa-heart');
+	});
+
+	$('#wishlist2').on('click', function(e){
+		e.preventDefault();
+		$('#wishlist2').toggleClass('fa-heart-o');
+		$('#wishlist2').addClass('fa-heart');
+	});
+
+	$('#wishlist3').on('click', function(e){
+		e.preventDefault();
+		$('#wishlist3').toggleClass('fa-heart-o');
+		$('#wishlist3').addClass('fa-heart');
+	});
+
+	$('#wishlist4').on('click', function(e){
+		e.preventDefault();
+		$('#wishlist4').toggleClass('fa-heart-o');
+		$('#wishlist4').addClass('fa-heart');
+	});
 });
