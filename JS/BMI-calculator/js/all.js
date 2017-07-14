afb7e38da14612653a262c781fdeb5fb98b47fe4
Bearkern <haolin1224@gmail.com>
2017-07-14 11:10:30 +0800

    Kaohsiung tour guide- AJAX

---
+++ JS/BMI-calculator/js/all.js
@@ -0,0 +1,9 @@
+var height = document.getElementById('heightId').value;
+var weight = document.getElementById('weightId').value;
+var seeResult = document.getElementById('seeResultId');
+
+seeResult.addEventListener('click', updateData, false);
+
+function updateData() {
+  
+}
